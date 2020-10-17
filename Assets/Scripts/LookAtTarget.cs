eae4a7d54ac0ab4a5ada04e5203af6d951d06cc4
AUniqueTim <43281258+AUniqueTim@users.noreply.github.com>
2020-10-17 16:51:03 -0400

    Revert "Changed player model head object name. Tried using LookAt Script but just makes enemy object seizure in place."
    This reverts commit 5732a09bb1d9419aca8370c69bb23cf985d0fefe.

--- Assets/Scripts/LookAtTarget.cs
+++ Assets/Scripts/LookAtTarget.cs
@@ -6,11 +6,11 @@ public class LookAtTarget : MonoBehaviour
 {
     public Transform target;
     public int speed;
-    
+    public int i;
     public void Update()
     {
         //transform.LookAt(target);
-        
+        //for (int i = 0; i >= 0; i++);
         transform.Translate(Vector3.forward * speed);
     }
 }
